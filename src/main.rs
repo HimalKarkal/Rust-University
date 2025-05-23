fe5a988b1e1fcd2e185429f8ad5ed7414f6d984c
Himal Karkal <karkalhimal@gmail.com>
2025-05-23 23:04:23 +1000

    Number guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,72 @@
-// Listing the first 10000 prime numbers
-// Imports
-use std::time::Instant;
+// Coding a number guessing game in Rust as in chapter 2 of the book
 
-fn primes_to_100000() {
-    // Starting the timer
-    let start = Instant::now();
+// Importing necessary crates
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::{thread, time};
 
-    let mut primes_vector: Vec<u32> = vec![2];
-    let mut current_number: u32 = 3;
+// Main function
 
-    while primes_vector.len() < 100000 {
+fn main() {
+    // Generate a random number between 1 and 10
+    let secret_number: u8 = rand::rng().random_range(1..=10);
+
+    // Print a welcome message
+    println!("Welcome to the number guessing game!");
+    println!("Guess the number between 1 and 10 that I am thinking of...");
+
+    // Declare a counter for the number of attempts
+    let mut attempts: u8 = 1;
+
+    // Loop until the user guesses the correct answer
+    loop {
+        // Creating a mutable variable to store the user's guess
+        let mut guess = String::new();
+
+        // Prompt the user for a guess
+        println!("Attempt {}: Please enter your guess: ", attempts);
+
+        // Read the user's input
+        match io::stdin()
+            .read_line(&mut guess) {
+                Ok(input) => input,
+                Err(_) => {
+                    println!("Error reading input. Please try again.");
+                    continue;
+                }
+            };
         
-        let mut is_prime: bool = true; // Creating a boolean variable to check if the current number is prime
-        for prime in &primes_vector {
-            if &current_number % prime == 0 {
-                is_prime = false;
-                break; // If the current number is divisible by any prime, it is not prime
+        // Convert the input to a number
+        let guess: u8 = match guess
+            .trim()
+            .parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Invalid input. Please enter a number between 1 and 10.");
+                    continue;
+                }
+            };
+        
+        // Compare the guess with the secret number using cmp::Ordering
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too low! Try again."),
+            Ordering::Greater => println!("Too high! Try again."),
+            Ordering::Equal => {
+                println!("Congratulations! You guess the number {} in {} attempts!", secret_number, attempts);
+                
+                // Introduce a delay before exiting
+                let delay = time::Duration::from_secs(3);
+                
+                println!("Exiting the game in 3 seconds...");
+                
+                thread::sleep(delay);
+                // Exit the loop
+                break;
             }
+        };
+        
+        // Increment the attempts counter
+        attempts += 1;
         }
-        if is_prime {
-            primes_vector.push(current_number);
-        }
-
-        current_number += 1;
-    }
-
-    println!("The 100,000th prime number is: {}.", primes_vector.get(99999).unwrap());
-
-    // Ending the timer
-    let duration = start.elapsed();
-    println!("Time taken to find the first 100,000 prime numbers is: {} ms.", duration.as_millis())
-}
-
-fn main(){
-    //Calling the function to find the first 100,000 prime numbers
-    primes_to_100000();
     }
