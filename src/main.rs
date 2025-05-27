6799bf53401579c8e716b8c6b43ff71b4cedc281
Himal Karkal <karkalhimal@gmail.com>
2025-05-27 20:44:19 +1000

    Built a simple command line calculator that can perform simple addition, subtraction, multiplication, and division.
    Used structs, impl for struct, enum, and match.
    
    Good practice problem :)

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,107 @@
-// Coding a number guessing game in Rust as in chapter 2 of the book
-
-// Importing necessary crates
+// Coding a simple calculator in Rust
+// It takes two numbers and a mathematical operator as input and outputs the result.
+// Importing necessary modules
 use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
 use std::{thread, time};
 
 // Main function
-
 fn main() {
-    // Generate a random number between 1 and 10
-    let secret_number: u8 = rand::rng().random_range(1..=10);
+    // DECLARING VARIABLES
+    let mut no_1: String = String::new();
+    let mut no_2: String = String::new();
+    let mut operator: String = String::new();
 
-    // Print a welcome message
-    println!("Welcome to the number guessing game!");
-    println!("Guess the number between 1 and 10 that I am thinking of...");
+    // PROMPTING USER FOR INPUT
+    println!("RUST CALCULATOR \n"); // Title
 
-    // Declare a counter for the number of attempts
-    let mut attempts: u8 = 1;
+    // Taking first number input
+    println!("Enter first number (float): ");
+    io::stdin()
+        .read_line(&mut no_1)
+        .expect("Failed to read line");
+    let no_1: f64 = no_1.trim().parse().expect("Invalid number!");
 
-    // Loop until the user guesses the correct answer
-    loop {
-        // Creating a mutable variable to store the user's guess
-        let mut guess = String::new();
+    // Taking operator input
+    println!("Enter operator (+,-,*,/): ");
+    io::stdin()
+        .read_line(&mut operator)
+        .expect("Failed to read line");
+    let operator: char = operator.trim().parse().expect("Invalid operator!");
 
-        // Prompt the user for a guess
-        println!("Attempt {}: Please enter your guess: ", attempts);
+    // Taking first number input
+    println!("Enter second number (float): ");
+    io::stdin()
+        .read_line(&mut no_2)
+        .expect("Failed to read line");
+    let no_2: f64 = no_2.trim().parse().expect("Invalid number!");
 
-        // Read the user's input
-        match io::stdin()
-            .read_line(&mut guess) {
-                Ok(input) => input,
-                Err(_) => {
-                    println!("Error reading input. Please try again.");
-                    continue;
+    // INSTANTIATING OPERATOR ENUM
+    let operator: Operation = match operator {
+        '+' => Operation::Add,
+        '-' => Operation::Subtract,
+        '*' => Operation::Multiply,
+        '/' => Operation::Divide,
+        _ => {
+            println!("Invalid operator! Please use +, -, *, or /");
+            return;
+        }
+    };
+    // CREATING CALCULATOR STRUCT
+    let calculator = Calculator {
+        no_1,
+        no_2,
+        operator
+    };
+
+    //CALLING THE CALCULATE METHOD
+    calculator.calculate();
+    println!("\nThank you for using the Rust Calculator!");
+    println!("Exiting in 3 seconds...");
+
+    // Adding a delay before exiting
+    let delay:u64 = 2;
+    let delay:time::Duration = time::Duration::from_secs(delay);
+    thread::sleep(delay);
+
+}
+
+enum Operation {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+}
+
+struct Calculator {
+    no_1: f64,
+    no_2: f64,
+    operator: Operation,
+}
+
+impl Calculator {
+    fn calculate(&self) {
+        match self.operator {
+            Operation::Add => {
+                println!("\nThe sum of {} and {} is: {}", self.no_1, self.no_2, self.no_1 + self.no_2);
+            },
+            Operation::Subtract => {
+                println!("\nThe difference of {} and {} is: {}", self.no_1, self.no_2, self.no_1 - self.no_2);
+            },
+            Operation::Multiply=> {
+                println!("\nThe product of {} and {} is: {}", self.no_1, self.no_2, self.no_1 * self.no_2);
+            },
+            Operation::Divide => {
+                match self.no_2 {
+                    0.0 => {
+                        println!("\nCannot divide by zero!");
+                    },
+                    _ => {
+                        println!("\nThe quotient of {} and {} is: {}", self.no_1, self.no_2, self.no_1 / self.no_2);
+                    },
                 }
-            };
-        
-        // Convert the input to a number
-        let guess: u8 = match guess
-            .trim()
-            .parse() {
-                Ok(num) => num,
-                Err(_) => {
-                    println!("Invalid input. Please enter a number between 1 and 10.");
-                    continue;
-                }
-            };
-        
-        // Compare the guess with the secret number using cmp::Ordering
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too low! Try again."),
-            Ordering::Greater => println!("Too high! Try again."),
-            Ordering::Equal => {
-                println!("Congratulations! You guess the number {} in {} attempts!", secret_number, attempts);
-                
-                // Introduce a delay before exiting
-                let delay = time::Duration::from_secs(3);
-                
-                println!("Exiting the game in 3 seconds...");
-                
-                thread::sleep(delay);
-                // Exit the loop
-                break;
-            }
-        };
-        
-        // Increment the attempts counter
-        attempts += 1;
+
+            },
+
         }
     }
+}
